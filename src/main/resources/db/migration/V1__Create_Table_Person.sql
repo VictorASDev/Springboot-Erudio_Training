1e7233a0f5ea87decaa4c96b922834ee0395c244
Victor <victor.augustosilva004@gmail.com>
2025-09-13 22:32:49 -0300

    introdução à migrations
    criação das primeiras migrations do banco

---
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `tb_person` (
+  `id` bigint(20) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+   PRIMARY KEY(id)
+);
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `tb_person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(2, 'São Paulo Brasil', 'Ayrton', 'Male', 'Senna'),
+(3, 'Anchiano Italy', 'Leonardo', 'Male', 'da Vinci'),
+(4, 'Porbandar India', 'Indira', 'Female', 'Gandhi'),
+(5, 'Porbandar India', 'Mahatma', 'Male', 'Gandhi'),
+(6, 'Kentucky US', 'Muhamamd', 'Male', 'Ali'),
+(7, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+(8, 'Mvezo South Africa', 'Nelson', 'Male', 'Mandela'),
+(9, 'Smiljan Croatia', 'Nikola', 'Male', 'Tesla');
