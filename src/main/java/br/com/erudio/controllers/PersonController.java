ae0fbf6d9e8ede124bdc63c82e539040fb76f66a
Victor <victor.augustosilva004@gmail.com>
2025-09-13 21:32:53 -0300

    fix: correção nos endpoints por versão da API

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -10,23 +10,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person")
 public class PersonController {
 
     @Autowired
     private PersonServices service;
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "v1/{id}")
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/v1",
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/v1",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
@@ -37,12 +39,13 @@ public class PersonController {
         return service.create(person);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/v1",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "v1/{id}")
     public void delete(@PathVariable("id") Long id) {
         service.delete(id);
         System.out.println("Entity with id " + id + " has been deleted!");
